39740326137626bfa895e991666e356c9bf90921
maredi-yamini <161504218+maredi-yamini@users.noreply.github.com>
2026-02-06 16:02:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-const roles = [
-    "Java Developer",
-    "Full Stack Developer"
-  ];
-  
-  let index = 0;
-  let charIndex = 0;
-  let currentText = "";
-  let isDeleting = false;
-  
-  function typeEffect() {
-    const typingSpan = document.querySelector(".typing");
-  
-    if (!isDeleting && charIndex <= roles[index].length) {
-      currentText = roles[index].substring(0, charIndex++);
-    } else if (isDeleting && charIndex >= 0) {
-      currentText = roles[index].substring(0, charIndex--);
-    }
-  
-    typingSpan.textContent = currentText;
-  
-    if (charIndex === roles[index].length) {
-      isDeleting = true;
-      setTimeout(typeEffect, 1000);
-      return;
-    }
-  
-    if (isDeleting && charIndex === 0) {
-      isDeleting = false;
-      index = (index + 1) % roles.length;
-    }
-  
-    setTimeout(typeEffect, isDeleting ? 60 : 120);
+const roles = ["Java & Full Stack Developer"];
+let index = 0;
+let charIndex = 0;
+let currentText = "";
+let isDeleting = false;
+
+function typeEffect() {
+  const typingSpan = document.querySelector(".typing");
+  const role = roles[index];
+
+  if (!isDeleting && charIndex <= role.length) {
+    currentText = role.substring(0, charIndex++);
+  } else if (isDeleting && charIndex >= 0) {
+    currentText = role.substring(0, charIndex--);
   }
-  
-  typeEffect();
-  
+
+  typingSpan.textContent = currentText;
+
+  if (charIndex === role.length) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1200);
+    return;
+  }
+
+  if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+  }
+
+  setTimeout(typeEffect, isDeleting ? 60 : 120);
+}
+
+typeEffect();
